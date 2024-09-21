e82fdee8003d224a1ec8b465f093a3c93e3f256a
skeercg <skeercg@gmail.com>
2024-09-21 15:52:25 +0500

    done

---
+++ assignment_3/assignment_3_1/assignment_3_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment_3_1
+//
+//  Created by Алихан Абихас on 21.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
